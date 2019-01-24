e566653b697ce30c8bc5bd2001bc93248419aeac
Xiaoda Wang <sig.xiaoda@gmail.com>
2019-01-23 18:05:37 -0800

    add local component state

--- src/App.js
+++ src/App.js
@@ -2,16 +2,45 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  }, {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  }
+];
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: list,
+      date: new Date()
+    };
+  }
   render() {
-    var helloWorld = "Welcome to the Road to learn React";
-    var user = {
-      firstName: "Bob",
-      lastName: "Smith"
-    }
     return (
       <div className="App">
-        <h2>{helloWorld}, {user.firstName} {user.lastName}</h2>
+        {
+          this.state.list.map(item => 
+              <div key={item.objectID}>
+                <span><a href={item.url}>{item.title}</a></span>
+                <span>{item.author}</span>
+                <span>{item.num_comments}</span>
+                <span>{item.points}</span>
+                <span>{this.state.date.toLocaleTimeString()}</span>
+              </div>
+        )}
       </div>
     );
   }
